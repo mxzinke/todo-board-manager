20a0f9f2589789c1397384d85fd120ff70e899d1
Maximilian Zinke <me@mxzinke.dev>
2019-06-08 01:33:10 +0200

    Adding API Interface

---
+++ src/Api.js
@@ -0,0 +1,66 @@
+import connectionConfig from '../api-config.json';
+
+export var API = new ApiConnector( JSON.parse(connectionConfig) );
+
+/* @class The Class for Connection to the RESTful-API */
+class ApiConnector {
+     constructor(params) {
+        this.address = params.apiUrl;
+        this.defaultMethod = params.defaultMethod;
+    }
+
+    /* @function Transpile the JSON Format into an Object
+     * @param request Requesting URL (without )
+     * @param method A HTTP Method, possible are GET, PUT, POST and DELETE (string)
+     * @return normal JS Object or an array of objects */
+    doRequest(request, method) {
+        var requestUrl = this.address + request;
+        var requestMethod = (method == undefined) ? this.defaultMethod : method;
+
+        try {
+            this._verifyRequest(requestUrl, requestMethod);
+        } catch {
+            return null;
+        }
+
+        // TODO: In here we do something to get the right information
+
+        var answer = ""; // something in JSON-Format
+
+        var decomposedAnswer = this.decomposeAnswer(answer);
+
+        try {
+            this._verifyAnswer(decomposedAnswer);
+        } catch {
+            return null;
+        }
+        
+        return decomposedAnswer;
+    }
+
+    /* @function Transpile the JSON Format into an Object
+     * @param answer JSON formated Answer 
+     * @return normal JS Object or an array of objects */
+    decomposeAnswer(answer) {
+        return JSON.parse(answer);
+    }
+
+    /* private @function Verifying the API-Request is not a wrong Request */
+    _verifyRequest(requestUrl, method) {
+        if (!["DELETE", "PUT", "POST", "GET"].some((element) => {
+            if (element === method) { return true; } else {return false; }
+        })) {
+            throw Error("The method \"" + method + "\" are not available.");
+        } else {
+            return true;
+        }
+    }
+
+    /* private @function Verifying the Answer including real information
+     * @param decomposedAnswer Usable Answer as an Object (as an Array) */
+    _verifyAnswer(decomposedAnswer) {
+        // TODO: try to find a bad request status
+
+        return true;
+    }
+}
