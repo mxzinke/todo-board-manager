c958899205e3243d485fd94c32f8b567eb083c4d
Maximilian Zinke <me@mxzinke.dev>
2019-06-08 20:47:49 +0200

    Fully functional Interface (some bugs)

--- src/components/ToDo-Element.js
+++ src/components/ToDo-Element.js
@@ -8,22 +8,27 @@ export default class ToDoElement extends React.Component {
     
     constructor(params) {
         super(params);
+        this.key = params.key;
         this.id = params.id;
         this.label = params.label;
         this.status = Boolean(params.state);
+        this.onChangeHandler = params.onChangeHandler;
     }
 
     onCheckboxChange() {
-        return null;
+        this.onChangeHandler();
+        this.status = !this.status;
     }
 
     render() {
-        //console.log(this.id, this.status)
         return (
-            <div className="ToDo-Element" id={"todo_" + this.id}>
+            <div className="ToDo-Element" id={"todo_" + this.key}>
                 <label className="container">
                     { (this.status === true) ? <span className="done">{ this.label }</span> : <span className="open">{ this.label }</span> } 
-                    { (this.status === true) ? <input type="checkbox" checked="checked" onChange={ event => this.onCheckboxChange() } /> : <input type="checkbox" onChange={ event => this.onCheckboxChange() } /> }
+                    { (this.status === true) ? 
+                    <input type="checkbox" checked="checked" onChange={ event => this.onCheckboxChange() } />
+                    : <input type="checkbox" onChange={ event => this.onCheckboxChange() } /> }
+
                     <span className="checkmark" />
                 </label>
             </div>
--- src/components/ToDo-Topic.js
+++ src/components/ToDo-Topic.js
@@ -4,26 +4,108 @@ import '../assets/styles/ToDo-Topic.css';
 //import Api from '../Api';
 
 /* @class Generating the topic based overview of the To-Do's 
- * @param id For generating DOM-Id and API-Request */
+ * @param key For generating DOM-Id and the ID for API-Request */
 export default class Topic extends React.Component {
 
     constructor(params) {
         super(params);
-        this.componentId = "topic-" + params.id;
+        this.componentId = "topic_" + params.id;
         this.topicId = params.id;
-    }  
+        this.state = {
+            title: "The Title Project",
+            open: [
+                {
+                    id: 124223322344,
+                    label: "Improve Design",
+                    index: 0
+                },
+                {
+                    id: 190929393912,
+                    label: "Setup the RESTful-API",
+                    index: 1
+                },
+                {
+                    id: 123929292992,
+                    label: "Implement RESTful-API Access",
+                    index: 2
+                }
+            ],
+            done: [
+                {
+                    id: 212444324344,
+                    label: "Adding basic React Components",
+                    index: 0
+                }
+            ]
+        };
+    }
+
+    /* @function Changing the State of a Element: done ←→ open
+     * @param elementId The Id of the Id which want to move
+     * @return true/false → if successful or not */
+    changeStateOfElement(elementId) {
+        var state = null;
+        var element = null;
+        var todoElements = this.state;
+        var elementIndex = null;
+
+        if (todoElements.open.some(e => e.id === elementId)) {
+            state = false;
+            element = todoElements.open.find(e => e.id === elementId);
+            elementIndex = todoElements.open.indexOf(element);
+        } else if (todoElements.done.some(e => e.id === elementId)) {
+            state = true;
+            element = todoElements.done.find(e => e.id === elementId);
+            elementIndex = todoElements.done.indexOf(element);
+        } else {
+            throw new Error("The Element searching for was not found!");
+        }
+
+        console.log("ElementIndex", element);
+
+        var highestIx = 0;
+
+        if (!state) {
+            // fixing up the index
+            todoElements.done.forEach(e => {
+                if (e.index > highestIx) { highestIx = e.index }
+            });
+            element.index = highestIx;
+            
+
+            // Add to new list and delete old element
+            todoElements.done.push(element);
+            delete todoElements.open[elementIndex];
+        } else {
+            // fixing up the index
+            todoElements.open.forEach(e => {
+                if (e.index > highestIx) { highestIx = e.index }
+            });
+            element.index = highestIx;
+
+            // Add to new list and delete old element
+            todoElements.open.push(element);
+            delete todoElements.done[elementIndex];
+        }
+
+        this.setState(todoElements);
+    }
 
     render() {
         return (
             <div className="Topic" id={this.componentId}>
-                <div className="TopicTitle">{this._getTitle()}</div>
-                <div className="ToDoElements" id="openToDo">
-                    <h2>Open ToDo's:</h2>
-                    { this._requestOpenToDos().map(element => <ToDoElement id={element.id} label={element.label} state={false} />) }
+                <div className="TopicTitle">{this.title}</div>
+                <div className="ToDoElements openToDo">
+                    <h2>Open ToDos:</h2>
+                    {
+                        this.state.open.map(
+                            element => <ToDoElement onChangeHandler={() => this.changeStateOfElement(element.id)} key={element.id} id={element.id} label={element.label} state={false} />)}
                 </div>
-                <div className="ToDoElements" id="doneToDo">
-                    <h2>Done ToDo's:</h2>
-                    { this._requestDoneToDos().map(element => <ToDoElement id={element.id} label={element.label} state={true} />) }
+                <div className="ToDoElements doneToDo">
+                    <h2>Done ToDos:</h2>
+                    {
+                        this.state.done.map(
+                            element => <ToDoElement onChangeHandler={() => this.changeStateOfElement(element.id)} key={element.id} id={element.id} label={element.label} state={true} />)}
                 </div>
             </div>
         );
@@ -31,48 +113,12 @@ export default class Topic extends React.Component {
 
     /* TODO: The following functions have to be connect to API @important */
 
-    /* private @function Loads all not done ToDo's by the actual topic from API */
-    _requestOpenToDos() {
-        var apiAnswer = [
-            {
-                id: 124223322344,
-                label: "Improve Design",
-                index: 0
-            },
-            {
-                id: 190929393912,
-                label: "Setup the RESTful-API",
-                index: 1
-            },
-            {
-                id: 123929292992,
-                label: "Implement RESTful-API Access",
-                index: 2
-            }
-        ];
+    /* @function For syncing the API with the local cache/storage
+     * If some changes were detected the application has to re-render */
+    syncToDoTopic() {
+        // Create Algorithm for syncing the API with local container (also title!)
 
         //var apiAnswer = Api.doRequest("topic/" + this.topicId + "?state=false", "GET");
-
-        return apiAnswer;
-    }
-
-    /* private @function Loads all done ToDo's by the actual topic from API */
-    _requestDoneToDos() {
-        return [
-            {
-                id: 212444324344,
-                label: "Adding basic React Components",
-                index: 3
-            }
-        ];
-
         //var apiAnswer = Api.doRequest("topic/" + this.topicId + "?state=false", "GET");
-    }
-
-    /* @function Loads the title of the topic from API */
-    _getTitle() {
-        return "Project: ToDo-Manager";
-
-        //var apiAnswer = Api.doRequest("topic/" + this.topicId + "/title", "GET");
     }
 }
