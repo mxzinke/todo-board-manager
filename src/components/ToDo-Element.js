4ed2b4a1c757ac43fb31cfbd62fbcfae948a8f56
Maximilian Zinke <me@mxzinke.dev>
2019-06-08 13:34:46 +0200

    Fixing React-Warning

--- src/components/ToDo-Element.js
+++ src/components/ToDo-Element.js
@@ -5,12 +5,16 @@ import '../assets/styles/ToDo-Element.css';
  * @param id Out of the API-Database and reused in DOM
  * @param label Content of the ToDo-Element */
 export default class ToDoElement extends React.Component {
+    
     constructor(params) {
         super(params);
         this.id = params.id;
         this.label = params.label;
-        console.log(this.id, params.state);
         this.status = Boolean(params.state);
+    }
+
+    onCheckboxChange() {
+        return null;
     }
 
     render() {
@@ -18,9 +22,9 @@ export default class ToDoElement extends React.Component {
         return (
             <div className="ToDo-Element" id={"todo_" + this.id}>
                 <label className="container">
-                { (this.status === true) ? <span className="done">{ this.label }</span> : <span className="open">{ this.label }</span> } 
-                { (this.status === true) ? <input type="checkbox" checked="checked"/> : <input type="checkbox" /> }
-                <span className="checkmark" />
+                    { (this.status === true) ? <span className="done">{ this.label }</span> : <span className="open">{ this.label }</span> } 
+                    { (this.status === true) ? <input type="checkbox" checked="checked" onChange={ event => this.onCheckboxChange() } /> : <input type="checkbox" onChange={ event => this.onCheckboxChange() } /> }
+                    <span className="checkmark" />
                 </label>
             </div>
         );
