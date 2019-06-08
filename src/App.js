bddefba05bb03c53f6786064772bb8b7af513652
Maximilian Zinke <me@mxzinke.dev>
2019-06-08 13:19:40 +0200

    Update App Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+/* @author Maximilian Zinke */
+
 import React from 'react';
 import './assets/styles/App.css';
 import Header from './components/Header';
+import Topic from './components/ToDo-Topic';
+//import Api from './Api';
 
+
+/* @important @function Root App Class */
 function App() {
   return (
     <div className="App">
-      <Header name="ToDo Manager with React" />
+      <Header name="ToDo Manager Board (build with React)" />
+      <div className="Topics">
+        <Topic id="01234567" />
+        <Topic id="01234567" />
+      </div>
     </div>
   );
 }
