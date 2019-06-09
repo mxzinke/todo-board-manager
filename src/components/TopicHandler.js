4e3848167ca3cd646e9fc419ba6c5786cd66e2d8
Maximilian Zinke <me@mxzinke.dev>
2019-06-09 18:40:00 +0200

    Delete Conformation

--- src/components/TopicHandler.js
+++ src/components/TopicHandler.js
@@ -11,11 +11,12 @@ export default class TopicHandler extends React.Component {
     }
 
     deleteTopic(topicId) {
+        if (!window.confirm("Do you really want to 𝗱𝗲𝗹𝗲𝘁𝗲 this Topic forever (a very long time)?")) {
+            return null;
+        }
+
         var topics = this.state.topics;
         var topicIx = topics.findIndex(e => e === topicId);
-
-        console.log("Delete the Topic", topicId, "With Index", topicIx);
-        console.log(topics);
         
         if (topicIx !== -1) {
             delete topics[topicIx];
@@ -26,14 +27,13 @@ export default class TopicHandler extends React.Component {
         } else {
             throw new Error("The Topic could not be found in the in the state-list.");
         }
-            
-        
     }
 
     render() {
         return (
             <div className="Topics">
                 {this.state.topics.map(topicId => <Topic id={topicId} onDeleteHandler={ (topicId) => this.deleteTopic(topicId) } />)}
+                <button className="AddButton" type="submit" onClick={ () => this.addTopic() }>+</button>
             </div>
         )
     }
